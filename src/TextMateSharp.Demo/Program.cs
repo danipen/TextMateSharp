793883ed58f1ac4536129c631f2c87732b831e1e
Daniel <dpenalba@codicesoftware.com>
2021-06-30 11:00:18 +0200

    Added more grammars to the Demo

--- src/TextMateSharp.Demo/Program.cs
+++ src/TextMateSharp.Demo/Program.cs
@@ -50,7 +50,7 @@ namespace TextMateSharp
                 Registry.Registry registry = new Registry.Registry(options);
 
                 int ini = Environment.TickCount;
-                IGrammar grammar = registry.LoadGrammar("source.cs");
+                IGrammar grammar = registry.LoadGrammar("source.php");
                 Console.WriteLine("Loaded {0} in {1}ms.",
                     Path.GetFileName(grammarFile),
                     Environment.TickCount - ini);
@@ -85,7 +85,14 @@ namespace TextMateSharp
 
                         foreach (string scopeName in token.Scopes)
                         {
+                            if (scopeName == "keyword.control.foreach.php")
+                            {
+
+                            }
+
+
                             Theme theme = registry.GetTheme();
+
                             List<ThemeTrieElementRule> themeRules =
                                 theme.Match(scopeName);
 
@@ -93,10 +100,12 @@ namespace TextMateSharp
                             {
                                 Console.WriteLine(
                                     "      - Matched theme rule: " +
-                                    "[bg: {0}, fg:{1}, fontStyle: {2}]",
+                                    "[bg: {0}, fg:{1}, fontStyle: {2}, scopeDeph: {3}, parentScopes: {4}]",
                                     theme.GetColor(themeRule.background),
                                     theme.GetColor(themeRule.foreground),
-                                    themeRule.fontStyle);
+                                    themeRule.fontStyle,
+                                    themeRule.scopeDepth,
+                                    string.Join(", ", themeRule.parentScopes == null ? new string[] { } : themeRule.parentScopes));
                             }
                         }
                     }
@@ -125,7 +134,19 @@ namespace TextMateSharp
 
             public string GetFilePath(string scopeName)
             {
-                return _grammarFile;
+                if (scopeName == "text.html.cshtml")
+                    return _grammarFile;
+
+                if (scopeName == "./dark_vs.json")
+                    return Path.Combine(Path.GetDirectoryName(_themeFile), "dark_vs.json");
+
+                if (scopeName == "text.html.basic")
+                    return Path.Combine(Path.GetDirectoryName(_grammarFile), "html.tmLanguage.json");
+
+                if (scopeName == "source.php")
+                    return Path.Combine(Path.GetDirectoryName(_grammarFile), "php.tmLanguage.json");
+
+                return null;
             }
 
             public ICollection<string> GetInjections(string scopeName)
