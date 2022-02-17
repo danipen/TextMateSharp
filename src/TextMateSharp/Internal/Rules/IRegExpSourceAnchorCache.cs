b280b881cd7d8b8af6e0768c1dfd48e3d1cfe4ad
Daniel <dpenalba@codicesoftware.com>
2022-02-17 13:35:28 +0100

    Fixed wrong assignment

--- src/TextMateSharp/Internal/Rules/IRegExpSourceAnchorCache.cs
+++ src/TextMateSharp/Internal/Rules/IRegExpSourceAnchorCache.cs
@@ -9,10 +9,10 @@ namespace TextMateSharp.Internal.Rules
 
         public IRegExpSourceAnchorCache(string a0_G0, string a0_G1, string a1_G0, string a1_G1)
         {
-            A0_G0 = A0_G0;
-            A0_G1 = A0_G1;
-            A1_G0 = A1_G0;
-            A1_G1 = A1_G1;
+            A0_G0 = a0_G0;
+            A0_G1 = a0_G1;
+            A1_G0 = a1_G0;
+            A1_G1 = a1_G1;
         }
     }
 }
