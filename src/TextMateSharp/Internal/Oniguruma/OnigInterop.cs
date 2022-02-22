87519df53b68996c04de3aed6d04db2163de02ac
Daniel <dpenalba@codicesoftware.com>
2022-02-22 14:12:27 +0100

    Adjust interop class

--- src/TextMateSharp/Internal/Oniguruma/OnigInterop.cs
+++ src/TextMateSharp/Internal/Oniguruma/OnigInterop.cs
@@ -9,16 +9,16 @@ namespace TextMateSharp.Internal.Oniguruma
         internal static extern IntPtr onigwrap_create(char* pattern, int len, int ignoreCase, int multiline);
 
         [DllImport("onigwrap", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr onigwrap_region_new();
+        
+        [DllImport("onigwrap", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void onigwrap_region_free(IntPtr region);
 
         [DllImport("onigwrap", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
         internal static extern void onigwrap_free(IntPtr regex);
 
         [DllImport("onigwrap", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        internal static extern int onigwrap_index_in(IntPtr regex, char* text, int offset, int length);
-
-        [DllImport("onigwrap", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-        internal static extern IntPtr onigwrap_search(IntPtr regex, char* text, int offset, int length);
+        internal static extern int onigwrap_search(IntPtr regex, char* text, int offset, int length, IntPtr region);
 
         [DllImport("onigwrap", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
         internal static extern int onigwrap_num_regs(IntPtr region);
