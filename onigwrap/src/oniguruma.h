6adc0ba0907349fa0abeb7b98f536bc0b724295d
Daniel <dpenalba@codicesoftware.com>
2022-10-24 13:20:23 +0200

    Update to last oniguruma.h version

--- onigwrap/src/oniguruma.h
+++ onigwrap/src/oniguruma.h
@@ -36,9 +36,9 @@ extern "C" {
 #define ONIGURUMA
 #define ONIGURUMA_VERSION_MAJOR   6
 #define ONIGURUMA_VERSION_MINOR   9
-#define ONIGURUMA_VERSION_TEENY   7
+#define ONIGURUMA_VERSION_TEENY   8
 
-#define ONIGURUMA_VERSION_INT     60907
+#define ONIGURUMA_VERSION_INT     60908
 
 #ifndef P_
 #if defined(__STDC__) || defined(_WIN32)
@@ -401,8 +401,9 @@ typedef unsigned int        OnigOptionType;
 #define ONIG_OPTION_NOT_END_STRING       (ONIG_OPTION_NOT_BEGIN_STRING << 1)
 #define ONIG_OPTION_NOT_BEGIN_POSITION   (ONIG_OPTION_NOT_END_STRING << 1)
 #define ONIG_OPTION_CALLBACK_EACH_MATCH  (ONIG_OPTION_NOT_BEGIN_POSITION << 1)
+#define ONIG_OPTION_MATCH_WHOLE_STRING   (ONIG_OPTION_CALLBACK_EACH_MATCH << 1)
 
-#define ONIG_OPTION_MAXBIT               ONIG_OPTION_CALLBACK_EACH_MATCH
+#define ONIG_OPTION_MAXBIT               ONIG_OPTION_MATCH_WHOLE_STRING
 
 #define ONIG_OPTION_ON(options,regopt)      ((options) |= (regopt))
 #define ONIG_OPTION_OFF(options,regopt)     ((options) &= ~(regopt))
@@ -531,6 +532,7 @@ ONIG_EXTERN OnigSyntaxType*   OnigDefaultSyntax;
 #define ONIG_SYN_ISOLATED_OPTION_CONTINUE_BRANCH (1U<<10) /* ..(?i)...|... */
 #define ONIG_SYN_VARIABLE_LEN_LOOK_BEHIND        (1U<<11)  /* (?<=a+|..) */
 #define ONIG_SYN_PYTHON                          (1U<<12)  /* \UHHHHHHHH */
+#define ONIG_SYN_WHOLE_OPTIONS                   (1U<<13)  /* (?Ie) */
 
 /* syntax (behavior) in char class [...] */
 #define ONIG_SYN_NOT_NEWLINE_IN_NEGATIVE_CC      (1U<<20) /* [^...] */
@@ -600,6 +602,7 @@ ONIG_EXTERN OnigSyntaxType*   OnigDefaultSyntax;
 #define ONIGERR_END_PATTERN_WITH_UNMATCHED_PARENTHESIS       -117
 #define ONIGERR_END_PATTERN_IN_GROUP                         -118
 #define ONIGERR_UNDEFINED_GROUP_OPTION                       -119
+#define ONIGERR_INVALID_GROUP_OPTION                         -120
 #define ONIGERR_INVALID_POSIX_BRACKET_TYPE                   -121
 #define ONIGERR_INVALID_LOOK_BEHIND_PATTERN                  -122
 #define ONIGERR_INVALID_REPEAT_RANGE_PATTERN                 -123
