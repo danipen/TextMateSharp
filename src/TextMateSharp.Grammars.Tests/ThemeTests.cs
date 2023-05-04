16e779c68dd5cd1d0b4ba2766e9a91d69d702882
Daniel <dpenalba@codicesoftware.com>
2023-05-04 10:09:35 +0200

    Add some tests to ensure that all the themes can be parsed

---
+++ src/TextMateSharp.Grammars.Tests/ThemeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using NUnit.Framework;
+using TextMateSharp.Themes;
+
+namespace TextMateSharp.Grammars.Tests
+{
+    public class ThemeTests
+    {
+        [Test]
+        public void Assert_Every_Theme_Can_Be_Parsed()
+        {
+            RegistryOptions options = new RegistryOptions(ThemeName.Light);
+
+            foreach (var themeName in Enum.GetValues<ThemeName>())
+            {
+                try
+                {
+                    IRawTheme theme = options.LoadTheme(themeName);
+                    Assert.That(theme.GetTokenColors(), Has.Count, "Failed: " + themeName);
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail(
+                        string.Format("[{0} theme]: {1}", themeName, ex.Message));
+                }
+            }
+        }
+    }
+}
